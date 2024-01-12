d019aa2b0f4af42ea4e88be915a2465d9871b666
bartooo <bcywinski11@gmail.com>
2024-01-12 22:31:52 +0100

    optimize queries

--- backend/db.py
+++ backend/db.py
@@ -48,54 +48,43 @@ class DB:
         url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
         self._engine = create_engine(url)
 
-    def get_first_n_points(self, n: int) -> List[DBPoint]:
-        query = """
-        SELECT way
-        FROM planet_osm_point
-        LIMIT %s;
-        """
-        gdf = gpd.GeoDataFrame.from_postgis(
-            query,
-            self._engine,
-            geom_col="way",
-            params=(n,),
-        ).to_crs("EPSG:4326")
-        return [DBPoint(gpd, row.way.x, row.way.y) for idx, row in gdf.iterrows()]
-
     def get_point_by_id(self, id: int) -> DBPoint:
-        query = """
+        query = f"""
         SELECT *
         FROM planet_osm_point
-        WHERE osm_id = %s;
+        WHERE osm_id = {id};
         """
         gdf = gpd.GeoDataFrame.from_postgis(
             query,
             self._engine,
             geom_col="way",
-            params=(id,),
         ).to_crs("EPSG:4326")
         return DBPoint(gdf.index[0], gdf.iloc[0].way.x, gdf.iloc[0].way.y)
 
-    def get_nearest_point(self, point: DBPoint) -> DBPoint:
-        query = """
-        WITH distances AS (
-            SELECT
-                *,
-                ST_Distance(ST_Transform(way, 4326), ST_SetSRID(ST_MakePoint(%s, %s), 4326)) AS distance
-            FROM planet_osm_point
-        )
-        SELECT *
-        FROM distances
-        ORDER BY distance ASC
-        LIMIT 1;
-        """
-        gdf = gpd.GeoDataFrame.from_postgis(
-            query,
-            self._engine,
-            geom_col="way",
-            index_col="osm_id",
-            params=(point.x, point.y),
-        ).to_crs("EPSG:4326")
+    def get_nearest_point(self, point: DBPoint) -> Optional[DBPoint]:
+        curr_radius = INIT_BUFFER_RADIUS
+        gdf = None
+        while (curr_radius < MAX_BUFFER_RADIUS) and (gdf is None or gdf.shape[0] == 0):
+            query = f"""
+            WITH nearest_point AS (
+                SELECT *, ST_Distance(ST_Transform(way, 4326), ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326)) AS distance
+                FROM planet_osm_point
+                WHERE ST_DWithin(ST_Transform(way, 4326), ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326), {curr_radius})
+                ORDER BY distance ASC
+                LIMIT 1
+            )
+            SELECT * FROM nearest_point;
+            """
+            gdf = gpd.GeoDataFrame.from_postgis(
+                query,
+                self._engine,
+                geom_col="way",
+                index_col="osm_id",
+            )
+            curr_radius *= 2
+        if not len(gdf):
+            return None
+        gdf = gdf.to_crs("EPSG:4326")
         return DBPoint(gdf.index[0], gdf.iloc[0].way.x, gdf.iloc[0].way.y)
 
     def find_shortest_path_between(self, A: DBPoint, B: DBPoint) -> Optional[Tuple[List[DBPoint], float]]:
@@ -107,16 +96,16 @@ class DB:
         tuple_string = tuple_string[:-1]
         tuple_string += ")"
         query = f"""
-        select *
-        from pgr_dijkstra(
-            'select osm_id as id, source, target, ST_Length(way) as cost from planet_osm_line where highway IN {tuple_string}',
+        SELECT *
+        FROM pgr_dijkstra(
+            'select osm_id as id, source, target, ST_Length(way) as cost FROM planet_osm_line where highway IN {tuple_string}',
             {source},
             {target},
             FALSE
         ) as p
-            left join planet_osm_line as r on p.edge = r.osm_id
-            left join planet_osm_line_vertices_pgr as pnt on p.node = pnt.id
-        order by p.seq;
+            LEFT JOIN planet_osm_line as r on p.edge = r.osm_id
+            LEFT JOIN planet_osm_line_vertices_pgr as pnt on p.node = pnt.id
+        ORDER BY p.seq;
         """
         try:
             gdf = gpd.GeoDataFrame.from_postgis(
@@ -124,91 +113,82 @@ class DB:
                 self._engine,
                 geom_col="the_geom",
                 index_col="osm_id",
-            ).to_crs("EPSG:4326")
+            )
             gdf = gdf.reset_index()
             return (
-                [DBPoint(row.osm_id, row.the_geom.x, row.the_geom.y) for idx, row in gdf.iterrows()],
+                [
+                    DBPoint(row.osm_id, row.the_geom.x, row.the_geom.y)
+                    for idx, row in gdf.to_crs("EPSG:4326").iterrows()
+                ],
                 gdf.iloc[-1].agg_cost,
             )
         except ValueError:
             return None, None
 
     def _find_nearest_source(self, start: DBPoint, end: DBPoint) -> int:
-        query = f"""
-        WITH distances1 as (
-        SELECT r.osm_id, ST_Distance(
-            ST_SetSRID(ST_StartPoint(r.way), 3851),
-            ST_SetSRID((
-                SELECT way
-                FROM planet_osm_point
-                WHERE osm_id = {start.id}
-                ), 3851)
-            ) as distance
-        FROM planet_osm_line as r
-        WHERE r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
-        )
-
-        select source from (
-            select *
-            from planet_osm_line r join distances1 d on (r.osm_id = d.osm_id)
-            where r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
-            order by d.distance asc limit 10
-            ) as closest_starts
-        order by ST_Distance(
-            ST_SetSRID(ST_EndPoint(closest_starts.way), 3851),
-            ST_SetSRID((
-                SELECT way
-                FROM planet_osm_point
-                WHERE osm_id = {end.id}
-                ), 3851)
-            ) limit 1;
-        """
+        curr_radius = INIT_BUFFER_RADIUS
+        result = None
         with self._engine.connect() as connection:
-            result = connection.execute(text(query)).fetchone()
-            if result:
-                return result[0]
-            else:
-                return None
-
-    def _find_nearest_target(self, end: DBPoint) -> int:
-        query = f"""
-        WITH distances as (
-            SELECT r.osm_id, ST_Distance(
-                ST_SetSRID(ST_EndPoint(r.way), 3851),
-                ST_SetSRID((
-                    SELECT way
+            while curr_radius < MAX_BUFFER_RADIUS and (result is None or len(result) == 0):
+                query = f"""
+                WITH start_point AS (
+                    SELECT ST_Transform(way, 4326) AS geom
+                    FROM planet_osm_point
+                    WHERE osm_id = {start.id}
+                ),
+                end_point AS (
+                    SELECT ST_Transform(way, 4326) AS geom
                     FROM planet_osm_point
                     WHERE osm_id = {end.id}
-                    ), 3851)
-                ) as distance
-            FROM planet_osm_line as r
-            where r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
-        )
-        select r.target
-        from planet_osm_line r join distances d on (r.osm_id = d.osm_id)
-        where r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
-        order by d.distance asc limit 1;
-        """
-        with self._engine.connect() as connection:
-            result = connection.execute(text(query)).fetchone()
-            if result:
-                return result[0]
-            else:
-                return None
+                ),
+                distances1 AS (
+                    SELECT r.osm_id, ST_Distance(ST_Transform(ST_StartPoint(r.way), 4326), sp.geom) AS distance
+                    FROM planet_osm_line AS r, start_point AS sp
+                    WHERE r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
+                    AND ST_DWithin(ST_Transform(ST_StartPoint(r.way), 4326), sp.geom, {curr_radius})
+                    ORDER BY distance ASC
+                )
+                SELECT source FROM (
+                    SELECT r.*, d.distance
+                    FROM planet_osm_line AS r
+                    JOIN distances1 d ON r.osm_id = d.osm_id
+                    WHERE r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
+                ) AS closest_starts
+                ORDER BY ST_Distance(ST_Transform(ST_EndPoint(closest_starts.way), 4326), (SELECT geom FROM end_point))
+                LIMIT 1;
+                """
+                result = connection.execute(text(query)).fetchone()
+                curr_radius *= 2
+            return result[0]
 
-    # def get_vertex_by_id(self, id):
-    #     query = """
-    #     SELECT *
-    #     FROM planet_osm_roads_vertices_pgr
-    #     WHERE id = %s;
-    #     """
-    #     gdf = gpd.GeoDataFrame.from_postgis(
-    #         query,
-    #         self._engine,
-    #         geom_col="the_geom",
-    #         params=(id,),
-    #     ).to_crs("EPSG:4326")
-    #     return gdf
+    def _find_nearest_target(self, end: DBPoint) -> int:
+        curr_radius = INIT_BUFFER_RADIUS
+        result = None
+        with self._engine.connect() as connection:
+            while curr_radius < MAX_BUFFER_RADIUS and (result is None or len(result) == 0):
+                query = f"""
+                WITH end_point AS (
+                SELECT ST_Transform(way, 4326) as geom
+                FROM planet_osm_point
+                WHERE osm_id = {end.id}
+                ),
+                nearby_lines AS (
+                    SELECT r.osm_id, r.way
+                    FROM planet_osm_line AS r
+                    CROSS JOIN end_point
+                    WHERE r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
+                    AND ST_DWithin(ST_Transform(ST_EndPoint(r.way), 4326), end_point.geom, {curr_radius})
+                )
+                SELECT r.target
+                FROM planet_osm_line AS r
+                JOIN nearby_lines AS d ON r.osm_id = d.osm_id
+                WHERE r.highway IN {tuple([str(r) for r in ROAD_TYPES])}
+                ORDER BY ST_Distance(ST_Transform(ST_EndPoint(d.way), 4326), (SELECT geom FROM end_point)) ASC
+                LIMIT 1;
+                """
+                result = connection.execute(text(query)).fetchone()
+                curr_radius *= 2
+            return result[0]
 
     def get_valid_points(
         self, point: DBPoint, max_distance: float, max_time: float, min_time: float, amenity: str
@@ -218,36 +198,36 @@ class DB:
         min_distance = VELOCITY * min_time
         curr_radius = INIT_BUFFER_RADIUS
         gdf = None
-        while (curr_radius < MAX_BUFFER_RADIUS) and (111320 * curr_radius < max_distance) and (gdf is None or gdf.shape[0] == 0):
+        while (
+            (curr_radius < MAX_BUFFER_RADIUS)
+            and (111320 * curr_radius < max_distance)
+            and (gdf is None or gdf.shape[0] == 0)
+        ):
             query = f"""
-            SELECT *, 111320 * ST_Distance(ST_Transform(bp.way, 4326), ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326)) as dist
-            FROM (
-                SELECT *
-                FROM planet_osm_point
-                WHERE ST_Within(
-                    ST_Transform(way, 4326), (
-                    SELECT ST_Buffer(
-                        ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326),
-                        {curr_radius}
-                    )))
-            ) as bp
-            WHERE 111320 * ST_Distance(ST_Transform(bp.way, 4326), ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326)) < {max_distance} AND 111320 * ST_Distance(ST_Transform(bp.way, 4326), ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326)) > {min_distance} AND bp.amenity = '{amenity}'
+            WITH point_geom AS (
+            SELECT ST_SetSRID(ST_MakePoint({point.x}, {point.y}), 4326) AS geom
+            ),
+            buffered_points AS (
+                SELECT p.*, ST_Distance(ST_Transform(p.way, 4326), (SELECT geom FROM point_geom)) * 111320 AS dist
+                FROM planet_osm_point AS p
+                WHERE ST_DWithin(ST_Transform(p.way, 4326), (SELECT geom FROM point_geom), {curr_radius})
+                AND p.amenity = '{amenity}'
+            )
+
+            SELECT *
+            FROM buffered_points
+            WHERE dist BETWEEN {min_distance} AND {max_distance}
             ORDER BY dist ASC;
             """
-            try:
-                gdf = gpd.GeoDataFrame.from_postgis(
-                    query,
-                    self._engine,
-                    geom_col="way",
-                ).to_crs("EPSG:4326")
-                gdf = gdf.reset_index()[1:]  # first row is the point itself
-            except ValueError:
-                pass
-            finally:
-                curr_radius *= 2
-
+            gdf = gpd.GeoDataFrame.from_postgis(
+                query,
+                self._engine,
+                geom_col="way",
+            )
+            gdf = gdf.reset_index()[1:]  # first row is the point itself
+            curr_radius *= 2
         return (
-            [DBPoint(row.osm_id, row.way.x, row.way.y) for idx, row in gdf.iterrows()]
-            if gdf is not None
-            else None
+            [DBPoint(row.osm_id, row.way.x, row.way.y) for idx, row in gdf.to_crs("EPSG:4326").iterrows()]
+            if len(gdf)
+            else []
         )
